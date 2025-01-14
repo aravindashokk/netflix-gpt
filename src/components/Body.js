68c853bf4960ba94f7f7b03ddeb43785d050dd41
aravindashokk <aravindkayal@gmail.com>
2025-01-14 12:45:54 -0600

    feat(ui): update userslice with user information, add header to browse page, add signout feature and updateprofile API

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,26 +1,47 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Browse from './Browse'
 import { createBrowserRouter } from 'react-router-dom';
 import { RouterProvider } from 'react-router-dom';
 import Login from './Login';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from '../utils/Firebase';
+import { useDispatch } from 'react-redux';
+import { addUser, removeUser } from '../utils/userSlice';
 
 const Body = () => {
+    const dispatch = useDispatch();
 
     const appRouter = createBrowserRouter([
         {
-            path:"/",
+            path: "/",
             element: <Login />
         },
         {
-            path:"/browse",  //only when authenticated
+            path: "/browse",  //only when authenticated
             element: <Browse />,
         },
-    ])
-  return (
-    <div>
-        <RouterProvider router={appRouter} />
-    </div>
-  )
+    ]);
+
+
+    useEffect(() => {
+        //handle user store update
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+                const { uid, email, displayName,photoURL} = user;
+                //update my store for sign in case
+                dispatch(addUser({ uid: uid, email: email, displayName: displayName, photoURL:photoURL }));
+            } else {
+                // User is signed out
+                dispatch(removeUser());
+            }
+        });
+    }, [])
+
+    return (
+        <div>
+            <RouterProvider router={appRouter} />
+        </div>
+    )
 }
 
 export default Body
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,19 @@
 import React, { useRef, useState } from 'react';
 import Header from './Header';
 import { checkValidData } from '../utils/Validate';
-import { createUserWithEmailAndPassword,signInWithEmailAndPassword  } from "firebase/auth";
+import { createUserWithEmailAndPassword,signInWithEmailAndPassword,updateProfile  } from "firebase/auth";
 import { auth } from '../utils/Firebase';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { addUser } from '../utils/userSlice';
 
 const Login = () => {
     const [isSignInForm,setIsSignInForm] = useState(true);
     const [errorMessage,setErrorMessage] = useState(null);
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
 
+    const name = useRef(null);
     const email = useRef(null);
     const password = useRef(null);
 
@@ -23,7 +29,21 @@ const Login = () => {
                 .then((userCredential) => {
                     // Signed up 
                     const user = userCredential.user;
+                    updateProfile(user, {
+                        displayName: name.current.value , photoURL: "https://avatars.githubusercontent.com/u/40633956?v=4"
+                      }).then(() => {
+                        // Profile updated!
+                        const { uid, email, displayName,photoURL} = auth.currentUser;
+                        dispatch(addUser({ uid: uid, email: email, displayName: displayName, photoURL:photoURL }));
+                        navigate("/browse");
+                        
+                      }).catch((error) => {
+                        // An error occurred
+                        setErrorMessage(error.message);
+                        
+                      });
                     console.log(user);
+                    
                 })
                 .catch((error) => {
                     const errorCode = error.code;
@@ -38,6 +58,7 @@ const Login = () => {
                     // Signed in 
                     const user = userCredential.user;
                     console.log(user)
+                    navigate("/browse");
                     
                 })
                 .catch((error) => {
@@ -59,9 +80,9 @@ const Login = () => {
         </div>
     <form onSubmit={(e)=>e.preventDefault()} className='w-4/12  absolute  p-12 bg-black my-36 mx-auto right-0 left-0  rounded-lg bg-opacity-80 text-white'>
         <h1 className='font-bold text-3xl py-4'>{isSignInForm ? "Sign In" : "Sign Up"}</h1>
-        {!isSignInForm && <input type='text' placeholder='Full Name' className='p-4 my-4 w-full rounded-sm bg-gray-600' />}
+        {!isSignInForm && <input ref={name} type='text' placeholder='Full Name' className='p-4 my-4 w-full rounded-sm bg-gray-600' />}
         <input ref={email} type='text' placeholder='Email Address' className='p-4 my-4 w-full rounded-sm bg-gray-600' />
-        <input ref={password} type='text' placeholder='Password' className='p-4 my-4 w-full rounded-sm bg-gray-600' />
+        <input ref={password} type='password' placeholder='Password' className='p-4 my-4 w-full rounded-sm bg-gray-600' />
         <p className='text-red-500 font-bold text-lg py-2'>{errorMessage}</p>
         <button className='p-2 my-6 bg-red-700 w-full rounded-lg' onClick={handleButtonClick}>{isSignInForm ? "Sign In" : "Sign Up"}</button>
         <p className='py-4 cursor-pointer' onClick={toggleSignInForm}>{isSignInForm ? "New to Netflix? Sign Up Now" : "Already registered? Sign In Now"}</p>
