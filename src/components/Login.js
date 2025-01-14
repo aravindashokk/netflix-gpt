b8729fd52e473af12b02fa30569a100b2fc9ea1e
aravindashokk <aravindkayal@gmail.com>
2025-01-14 11:31:33 -0600

    feat(ui): Implement Sign In user API and Created Redux store with UserSlice

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,8 @@
 import React, { useRef, useState } from 'react';
 import Header from './Header';
 import { checkValidData } from '../utils/Validate';
+import { createUserWithEmailAndPassword,signInWithEmailAndPassword  } from "firebase/auth";
+import { auth } from '../utils/Firebase';
 
 const Login = () => {
     const [isSignInForm,setIsSignInForm] = useState(true);
@@ -13,16 +15,47 @@ const Login = () => {
         //validate form data
         const message = checkValidData(email.current.value,password.current.value);
         setErrorMessage(message);
-    }
+
+        if(message) return;
+        if(!isSignInForm) {
+            //Sign Up Login
+            createUserWithEmailAndPassword(auth, email.current.value,password.current.value)
+                .then((userCredential) => {
+                    // Signed up 
+                    const user = userCredential.user;
+                    console.log(user);
+                })
+                .catch((error) => {
+                    const errorCode = error.code;
+                    const errorMessage = error.message;
+                    setErrorMessage(errorCode + "-" +errorMessage);
+                });
+        }
+        else{
+            //Sign In Logic
+            signInWithEmailAndPassword(auth, email.current.value,password.current.value)
+                .then((userCredential) => {
+                    // Signed in 
+                    const user = userCredential.user;
+                    console.log(user)
+                    
+                })
+                .catch((error) => {
+                    const errorCode = error.code;
+                    const errorMessage = error.message;
+                    setErrorMessage(errorCode + "-" +errorMessage);
+                });
+        }
+    };
 
     const toggleSignInForm = () => {
         setIsSignInForm(!isSignInForm);
-    }
+    };
   return (
     <div>
         <Header />
         <div className='absolute'>
-            <img className='bg-gradient-to-b from-black' src="https://assets.nflxext.com/ffe/siteui/vlv3/154a9550-ce07-4e28-819c-63185dd849f8/web/US-en-20250106-TRIFECTA-perspective_65e335d4-6f1e-4d03-8daa-e439fbaaa340_large.jpg" srcset="https://assets.nflxext.com/ffe/siteui/vlv3/154a9550-ce07-4e28-819c-63185dd849f8/web/US-en-20250106-TRIFECTA-perspective_65e335d4-6f1e-4d03-8daa-e439fbaaa340_large.jpg 2000w, https://assets.nflxext.com/ffe/siteui/vlv3/154a9550-ce07-4e28-819c-63185dd849f8/web/US-en-20250106-TRIFECTA-perspective_65e335d4-6f1e-4d03-8daa-e439fbaaa340_medium.jpg 1279w, https://assets.nflxext.com/ffe/siteui/vlv3/154a9550-ce07-4e28-819c-63185dd849f8/web/US-en-20250106-TRIFECTA-perspective_65e335d4-6f1e-4d03-8daa-e439fbaaa340_small.jpg 959w" alt='bg-img' aria-hidden="true" class="default-ltr-cache-19j6xtr"/>
+            <img className='bg-gradient-to-b from-black' src="https://assets.nflxext.com/ffe/siteui/vlv3/154a9550-ce07-4e28-819c-63185dd849f8/web/US-en-20250106-TRIFECTA-perspective_65e335d4-6f1e-4d03-8daa-e439fbaaa340_large.jpg"  alt='bg-img' aria-hidden="true" />
         </div>
     <form onSubmit={(e)=>e.preventDefault()} className='w-4/12  absolute  p-12 bg-black my-36 mx-auto right-0 left-0  rounded-lg bg-opacity-80 text-white'>
         <h1 className='font-bold text-3xl py-4'>{isSignInForm ? "Sign In" : "Sign Up"}</h1>
