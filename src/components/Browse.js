353afb5a9038967a383ee58aecd9cc203e1a07ff
aravindashokk <aravindkayal@gmail.com>
2025-01-14 15:10:50 -0600

    feat(ui): getNowPlayingMovies data from TMDB movies list API, created movieSlice and add movie data to store

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,7 +1,26 @@
-import React from 'react'
-import Header from './Header'
+import React from 'react';
+import Header from './Header';
+import {useEffect} from 'react';
+import {API_OPTIONS} from "../utils/constant";
+import { useDispatch } from 'react-redux';
+import { addNowPlayingMovies } from '../utils/movieSlice';
+
 
 const Browse = () => {
+  const dispatch = useDispatch();
+
+  const getNowPlayingMovies = async () => {
+    const data = await fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', API_OPTIONS); //returns promise
+    const json = await data.json(); //convert to readable stream
+    console.log(json.results);
+    dispatch(addNowPlayingMovies(json.results));
+  };
+
+  useEffect(() => {
+    getNowPlayingMovies();
+  }, [])
+  
+
   return (
     <div>
       <Header />
