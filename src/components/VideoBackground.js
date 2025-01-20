7401b9dbefc9f37ce584bc7fabdf98c197364882
aravindashokk <aravindkayal@gmail.com>
2025-01-20 14:45:36 -0600

    feat(ui): add useMovieTrailer custom hook, dispatch addTrailerVideo action and subscribe to store via useSelector for trailerVideo

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -1,8 +1,12 @@
-import React from 'react'
+import { useSelector } from "react-redux";
+import useMovieTrailer from "../hooks/useMovieTrailer";
 
-const VideoBackground = () => {
+const VideoBackground = ( {movieId} ) => {
+  const trailerVideo = useSelector(store => store.movies?.trailerVideo);
+  useMovieTrailer(movieId);
+
   return (
-    <div>VideoBackground</div>
+    <div><iframe  src={"https://www.youtube.com/embed/"+trailerVideo?.key} title="YouTube video player"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin"></iframe></div>
   )
 }
 
--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -7,7 +7,7 @@ const VideoTitle = ({title,overview}) => {
         <p className="py-6 text-lg w-1/4">{overview}</p>
         <div className=''>
             <button className='bg-gray-500 text-white p-4 px-12 text-lg bg-opacity-50 rounded-lg'>▶️ Play</button>
-            <butoon className='mx-2 bg-gray-500 text-white p-4 px-12 text-lg bg-opacity-50 rounded-lg'>More Info</butoon>
+            <button className='mx-2 bg-gray-500 text-white p-4 px-12 text-lg bg-opacity-50 rounded-lg'>More Info</button>
         </div>
     </div>
   )
