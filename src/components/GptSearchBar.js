401ac54442221babc8782128eaad58f4b15dd508
aravindashokk <aravindkayal@gmail.com>
2025-01-21 11:44:50 -0600

    feat(ui): multilingual support added

--- src/components/GptSearchBar.js
+++ src/components/GptSearchBar.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import lang from '../utils/languageConstants'
+import { useSelector } from 'react-redux'
 
 const GptSearchBar = () => {
+  const lang_key = useSelector(store=>store.config.lang);
   return (
     <div className='pt-[10%] flex justify-center'>
         <form className='w-1/2 bg-black grid grid-cols-12'>
-            <input type='text' className='p-4 m-4 col-span-9' placeholder={lang.tamil.gptSearchPlaceholder} />
+            <input type='text' className='p-4 m-4 col-span-9' placeholder={lang[lang_key].gptSearchPlaceholder} />
             <button className='col-span-3 m-3 py-2 px-4 bg-red-700 text-white rounded-lg'>
-                {lang.tamil.search}
+                {lang[lang_key].search}
             </button>
         </form>
     </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -8,6 +8,7 @@ import { addUser,removeUser } from '../utils/userSlice';
 import { onAuthStateChanged } from 'firebase/auth';
 import { LOGO, SUPPORTED_LANGUAGES } from '../utils/constant';
 import { toggleGptSearchView } from '../utils/gptSlice';
+import { changeLanguage } from '../utils/configSlice';
 
 
 
@@ -50,12 +51,16 @@ const Header = () => {
     });
   }
 
+  const handleLanguageChange = (e) => {
+    dispatch(changeLanguage(e.target.value));
+  }
+
   return (
     <div className='absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between'>
       <img className='w-44' src={LOGO} alt='logo' />
       {/* load this portion only when user logged in */}
       {user && <div className='flex p-2'>
-        <select className='p-2 m-2 bg-gray-900 text-white rounded-lg'>
+        <select onChange={handleLanguageChange} className='p-2 m-2 bg-gray-900 text-white rounded-lg'>
             {SUPPORTED_LANGUAGES.map(lang=> <option key={lang.identifier} value={lang.identifier}>{lang.name}</option>)}
         </select>
         <button onClick={handleGptSearchClick} className='py-2 px-4 mx-4 my-2 bg-purple-800 text-white rounded-lg'>GPT Search</button>
